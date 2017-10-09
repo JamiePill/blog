923943999ea0f489f616989bcf216d5682383b64
JamiePill <JamiePill@users.noreply.github.com>
2017-10-09 20:35:34 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,7 +9,37 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [
+    'uses' => 'PostController@getIndex',
+    'as' => 'blog.index'
+]);
+Route::get('post/{id}', [
+    'uses' => 'PostController@getPost',
+    'as' => 'blog.post'
+]);
+Route::get('about', function () {
+    return view('other.about');
+})->name('other.about');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('', [
+        'uses' => 'PostController@getAdminIndex',
+        'as' => 'admin.index'
+    ]);
+    Route::get('create', [
+        'uses' => 'PostController@getAdminCreate',
+        'as' => 'admin.create'
+    ]);
+    Route::Post('create', [
+        'uses' => 'PostController@postAdminCreate',
+        'as' => 'admin.create'
+    ]);
+    Route::Get('edit/{id}', [
+        'uses' => 'PostController@getAdminEdit',
+        'as' => 'admin.edit'
+    ]);
+    
+    Route::Post('edit', [
+        'uses' => 'PostController@postAdminUpdate',
+        'as' => 'admin.update'
+    ]);
 });
