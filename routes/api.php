e7b0f892b5dff2b60e48f6ef782d3e8f15ee89d1
JamiePill <JamiePill@users.noreply.github.com>
2017-10-09 22:25:31 +0700

    Check error

--- routes/api.php
+++ routes/api.php
@@ -1,45 +1,18 @@
 <?php
+
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/', [
-    'uses' => 'PostController@getIndex',
-    'as' => 'blog.index'
-]);
-Route::get('post/{id}', [
-    'uses' => 'PostController@getPost',
-    'as' => 'blog.post'
-]);
-Route::get('about', function () {
-    return view('other.about');
-})->name('other.about');
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('', [
-        'uses' => 'PostController@getAdminIndex',
-        'as' => 'admin.index'
-    ]);
-    Route::get('create', [
-        'uses' => 'PostController@getAdminCreate',
-        'as' => 'admin.create'
-    ]);
-    Route::Post('create', [
-        'uses' => 'PostController@postAdminCreate',
-        'as' => 'admin.create'
-    ]);
-    Route::Get('edit/{id}', [
-        'uses' => 'PostController@getAdminEdit',
-        'as' => 'admin.edit'
-    ]);
-    
-    Route::Post('edit', [
-        'uses' => 'PostController@postAdminUpdate',
-        'as' => 'admin.update'
-    ]);
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
